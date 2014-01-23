422658beba21e8eb2090d2b2c183ee713700edd3
bissaias <bissaias@hotmail.com>
2014-01-23 14:52:32 +0200

    ignore

--- joblet1/src/main/java/com/joblet/one/Customer.java
+++ joblet1/src/main/java/com/joblet/one/Customer.java
@@ -1,7 +1,10 @@
 package com.joblet.one;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 //@Table(schema = "job1")
--- joblet2/src/main/java/com/joblet/two/Merchant.java
+++ joblet2/src/main/java/com/joblet/two/Merchant.java
@@ -1,7 +1,10 @@
 package com.joblet.two;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 //@Table(schema = "job2")
